74d13632550c52dbf9912b15cc0ab78dbb73d6e6
simon00102 <simon00102@gmail.com>
2021-04-15 14:00:46 +0900

    NICE custom

--- quantlibee.cpp
+++ quantlibee.cpp
@@ -4,29 +4,30 @@
 #include <string.h>
 #include <tchar.h>
 
-#include <ql/time/date.hpp>
-
+//#include <ql/time/date.hpp>
+#include "time/Date.hpp"
 #include "custview.h"
 
 #include "quantlibee.h"
 
-using namespace QuantLib;
+//using namespace QuantLib;
+using namespace NICELib;
 
-static HRESULT FormatQuantLibDate( QuantLib::Date *date, char *pResult, int result_size )
+static HRESULT FormatQuantLibDate( NICELib::Date *date, char *pResult, int result_size )
 {
-    wsprintf(pResult, "%d-%d-%d", date->year(), int(date->month()), date->dayOfMonth());
+    wsprintf(pResult, "%d-%d-%d", date->GetYear(), date->GetMonth(), date->GetDayofMonth());
     return S_OK;
 }
 
 HRESULT ADDIN_API AddIn_quantlibdate( DWORD dwAddress, DEBUGHELPER *pHelper, int nBase, BOOL bUniStrings, char *pResult, size_t max, DWORD reserved )
 {
-    QuantLib::Date date;
+    NICELib::Date date;
     DWORD nGot;
 
     // read date from debuggee memory space
-    if (pHelper->ReadDebuggeeMemoryEx(pHelper, pHelper->GetRealAddress(pHelper), sizeof(QuantLib::Date), &date, &nGot) != S_OK)
+    if (pHelper->ReadDebuggeeMemoryEx(pHelper, pHelper->GetRealAddress(pHelper), sizeof(NICELib::Date), &date, &nGot) != S_OK)
         return E_FAIL;
-    if (nGot != sizeof(QuantLib::Date))
+    if (nGot != sizeof(NICELib::Date))
         return E_FAIL;
 
     return FormatQuantLibDate(&date, pResult, max);
